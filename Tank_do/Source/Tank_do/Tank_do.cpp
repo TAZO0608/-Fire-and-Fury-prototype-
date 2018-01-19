62a429e437bd328fc1c970910576605af8399876
Dr.Game <j1981700608@hotmail.com>
2018-01-19 12:06:10 +0800

    landscape

---
+++ Tank_do/Source/Tank_do/Tank_do.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Tank_do.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Tank_do, "Tank_do" );
---
+++ Tank_do/Source/Tank_do/Tank_doGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "Tank_doGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TANK_DO_API ATank_doGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
